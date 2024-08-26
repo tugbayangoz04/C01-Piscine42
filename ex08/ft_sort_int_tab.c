bf667048be24e7f59101430142619897a7cbf4ae
tugbayangoz04 <143502388+tugbayangoz04@users.noreply.github.com>
2024-08-26 12:14:47 +0300

    Add files via upload

---
+++ ex08/ft_sort_int_tab.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_int_tab.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tyangoz <tyangoz@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/22 16:16:01 by tyangoz           #+#    #+#             */
+/*   Updated: 2024/08/22 16:25:51 by tyangoz          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_sort_int_tab(int *tab, int size)
+{
+	int	i;
+	int	swap;
+
+	while (size >= 0)
+	{
+		i = 0;
+		while (i < size - 1)
+		{
+			if (tab[i] > tab[i + 1])
+			{
+				swap = tab[i];
+				tab[i] = tab[i + 1];
+				tab[i + 1] = swap;
+			}
+			i++;
+		}
+		size--;
+	}
+}
